5b40fc88a952e754fe63a4f3336da6b31552410b
Ki Won <kyun4525@gmail.com>
2024-07-07 11:50:35 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -0,0 +1,23 @@
+function gcd(a, b) {
+    if (b === 0) {
+        return a;
+    } else if (a % b === 0) {
+        return b;
+    } else {
+        return gcd(b, a % b);
+    }
+}
+
+function lcm(a, b) {
+    return (a * b) / gcd(a, b);
+}
+
+function solution(arr) {
+    let answer = 1;
+    
+    for (let i = 0; i < arr.length; i++) {
+        answer = lcm(answer, arr[i]);
+    }
+    
+    return answer;
+}
