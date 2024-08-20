9b47718f224918cdb366886dc1add30ec69a8a98
Ki Won <kyun4525@gmail.com>
2024-08-20 16:03:43 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.06 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    let result = 0;
+    
+    while (n !== 0) {
+        if (n % 2 === 1) {
+            result++;
+            n--;
+        } else {
+            n /= 2;
+        }
+    }
+    
+    return result;
+}
