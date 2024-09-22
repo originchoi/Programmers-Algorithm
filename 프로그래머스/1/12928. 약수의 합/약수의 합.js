b697da531de110c6995b3b462dc6969b1cbf2eef
Ki Won <kyun4525@gmail.com>
2024-09-22 23:58:30 +0900

    [level 1] Title: 약수의 합, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -0,0 +1,17 @@
+function solution(n) {
+    let answer = 0;
+
+    if (n === 0) return 0;
+
+    for (let i = 1; i * i <= n; i++) {
+        if (n % i === 0) {
+            answer += i;
+            
+            if (i !== n / i) {
+                answer += n / i;
+            }
+        }
+    }
+    
+    return answer;
+}
